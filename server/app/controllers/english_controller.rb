aacfb366808f73aac52f166073894516836752b1
Jacob <jacobmjones17@gmail.com>
2022-12-02 16:10:02 -0500

    I added the sessions controller and I also added some information iside of some of the controllers.

--- server/app/controllers/english_controller.rb
+++ server/app/controllers/english_controller.rb
@@ -1,2 +1,12 @@
 class EnglishController < ApplicationController
+
+    def index
+        english_words = English.all
+
+        if session[:user_id]
+        render json: english_words
+        else
+        render json: { errors: ["Not authorized"] }, status: :unauthorized
+        end
+    end
 end
--- server/app/controllers/portuguese_controller.rb
+++ server/app/controllers/portuguese_controller.rb
@@ -1,2 +1,12 @@
 class PortugueseController < ApplicationController
+
+    def index
+        portuguese_words = Portuguese.all
+
+        if session[:user_id]
+        render json: portuguese_words
+        else
+        render json: { errors: ["Not authorized"] }, status: :unauthorized
+        end
+    end
 end
