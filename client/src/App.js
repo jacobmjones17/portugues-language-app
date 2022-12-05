385df6c5437dd73520f1067d8bfd53eaa119860e
Jacob <jacobmjones17@gmail.com>
2022-12-05 16:59:52 -0500

    I am the components to make sure state is the correct value

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { Routes, Route } from 'react-router-dom';
+import QuestionList from "./components/questions/QuestionList"
+import QuestionForm from "./components/questions/QuestionForm"
+import CardForm from "./components/flashcards/CardForm"
+import CardList from "./components/flashcards/CardList"
 import Home from "./components/Home";
 import './App.css';
 
 const App = () => {
-    const [recipes, setRecipes] = useState([]);
+    const [questions, setQuestions] = useState([]);
     const [user, setUser] = useState(null);
     const [loggedIn, setLoggedIn] = useState(false);
 
@@ -57,7 +61,7 @@ const App = () => {
 return (
     <div className="App">
     <Routes>
-    <Route path="quiz" element={<QuestionList />} />
+        <Route path="quiz" element={<QuestionList />} />
         <Route path="flashcards" element={<CardList />} />
         <Route path="addquestion" element={<QuestionForm />} />
         <Route path="addflashcard" element={<CardForm />} />
