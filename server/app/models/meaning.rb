3b4cdc4d771b5413020b3d49bfa4057c5e56085b
Jacob <jacobmjones17@gmail.com>
2022-12-20 19:07:08 -0500

    I added quiz_answers method to shuffle the answer choices

--- server/app/models/meaning.rb
+++ server/app/models/meaning.rb
@@ -2,4 +2,15 @@ class Meaning < ApplicationRecord
     has_many :questions
     belongs_to :english
     belongs_to :portuguese
+
+    def quiz_answers
+        quiz_obj = {
+            wrong_english_answers: English.joins(:meanings).where.not("meanings.definition = ?", "A way to greet someone"),
+            wrong_portuguese_answers: Portuguese.joins(:meanings).where.not("meanings.definition = ?", "A way to greet someone"),
+            correct_english_answers: self.englishes,
+            correct_portuguese_answers: self.portugueses
+        }
+    end
+    
 end
+
